4c4674f306dec7695f378cfe4cbbef5bd12cce4c
tonitomaatti <toni.turpeinen@helsinki.fi>
2021-01-08 01:42:13 +0200

    6.20 anectodeForm with connect

--- redux-anecdotes/src/components/AnectodeForm.js
+++ redux-anecdotes/src/components/AnectodeForm.js
@@ -1,18 +1,16 @@
 import React from 'react'
-
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnectodeForm = (props) => {
-  const dispatch = useDispatch()
 
   const newAnectode = async (event) => {
     event.preventDefault()
     const content = event.target.anectode.value
     event.target.anectode.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(setNotification('An anectode has been created', 5))
+    props.createAnecdote(content)
+    props.setNotification('An anectode has been created', 5)
   }
 
   return (
@@ -26,4 +24,14 @@ const AnectodeForm = (props) => {
   )
 }
 
-export default AnectodeForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+const ConnectedAnectodeForm = connect(
+  null,
+  mapDispatchToProps
+)(AnectodeForm)
+
+export default ConnectedAnectodeForm
